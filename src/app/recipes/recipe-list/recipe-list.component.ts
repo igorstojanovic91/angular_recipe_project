6de7a11e1cf6c4157b6a5f929cd1bbacc94000a7
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-07-25 19:55:35 +0200

    Editing Recipe details item and list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe("Pad Thai",
+      "Amazing Pad Thai",
+      "https://janinaandfood.com/wp-content/uploads/2020/06/B1044FC1-7EEE-478F-8FAE-664FD46555BF-4AE233AB-7532-45B8-9E8A-E7DC36AA06A3-min-1-1060x1059.jpg")
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
