6657c2b5e8f84d554013507e43ad3a6e70e505a8
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-07-31 10:14:32 +0200

    making recipe list and recipe detail talk to each other

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -10,12 +10,23 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("Pad Thai",
       "Amazing Pad Thai",
+      "https://janinaandfood.com/wp-content/uploads/2020/06/B1044FC1-7EEE-478F-8FAE-664FD46555BF-4AE233AB-7532-45B8-9E8A-E7DC36AA06A3-min-1-1060x1059.jpg"),
+  new Recipe("Another Pad Thai",
+      "Another Amazing Pad Thai",
       "https://janinaandfood.com/wp-content/uploads/2020/06/B1044FC1-7EEE-478F-8FAE-664FD46555BF-4AE233AB-7532-45B8-9E8A-E7DC36AA06A3-min-1-1060x1059.jpg")
   ];
+
+  @Output()
+  recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
