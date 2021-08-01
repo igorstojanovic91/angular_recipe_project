232e121c528b3684f09a8802f039422e7c46779e
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-08-01 10:17:28 +0200

    making dropdown click working better

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,18 +4,13 @@ import {Directive, ElementRef, HostBinding, HostListener, OnInit} from '@angular
   selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit {
-
   @HostBinding('class.open') isOpen = false;
-
-  constructor(private elementRef: ElementRef) { }
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
 
   ngOnInit(): void {
-  }
-
-  //open
-  @HostListener('click')
-  toggleOpen() {
-    this.isOpen = !this.isOpen;
   }
 
 }
