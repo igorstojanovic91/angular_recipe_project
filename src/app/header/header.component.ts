1065409225b815286445b8ca2e6a5c4c0c3f3907
Igor Stojanovic <i.stojanovic01@gmail.com>
2021-08-08 13:52:45 +0200

    Set up basic top-level routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,9 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  @Output()
-  fetureSelected: EventEmitter<string> = new EventEmitter<string>();
-  collapsed = true;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature: string) {
-    this.fetureSelected.emit(feature);
-  }
-
-
+export class HeaderComponent {
 }
